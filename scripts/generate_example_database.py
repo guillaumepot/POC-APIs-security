125dfcc413d9003a28ca030d2c467866690bd9b6
Guillaume Pot <guillaume.pot@outlook.com>
2025-03-21 17:14:59 +0000

    Update tree

--- scripts/generate_example_database.py
+++ scripts/generate_example_database.py
@@ -1,4 +1,8 @@
-#src/scripts/generate_example_database.py
+#scripts/generate_example_database.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 
 from config.config import DB_NAME, EXAMPLE_TABLES, EXAMPLE_DATA
 from modules.logger import LoggerManager
