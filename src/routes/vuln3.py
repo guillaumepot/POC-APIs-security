b822dd0951d1e74c0fcf24f8ca74aae65306ff7c
Guillaume Pot <guillaume.pot@outlook.com>
2025-03-28 11:45:14 +0000

    Dev vuln3 part 1 done

--- src/routes/vuln3.py
+++ src/routes/vuln3.py
@@ -17,14 +17,14 @@ vulnerability3 = APIRouter()
 """
 Routes Declaration
 """
-@vulnerability3.get("/vuln3/broken/activity/{id: int}", tags=["Vuln III"])
+@vulnerability3.get("/vuln3/broken/activity", tags=["Vuln III"])
 def get_user_activity_vuln(activity: str): #, current_user: dict = Depends(get_current_user)
     # Query
     query = """
             SELECT DISTINCT *
             FROM activity a
-            JOIN collaborators c
-            ON a.user_id = c.id
+            JOIN users u
+            ON a.user_id = u.id
             WHERE a.activity = ?;
             """
 
@@ -42,16 +42,33 @@ def get_user_activity_vuln(activity: str): #, current_user: dict = Depends(get_c
     return response
 
 
-@vulnerability3.get("/vuln3/fixed/activity/{id: int}", tags=["Vuln III"])
-def get_user_activity_fix(id: int, activity: str): #, current_user: dict = Depends(get_current_user)
-    pass
+@vulnerability3.get("/vuln3/fixed/activity", tags=["Vuln III"])
+def get_user_activity_fix(activity: str): #, current_user: dict = Depends(get_current_user)
+    # Query
+    query = """
+            SELECT DISTINCT u.username, a.activity, a.date, a.device
+            FROM activity a
+            JOIN users u
+            ON a.user_id = u.id
+            WHERE a.activity = ?;
+            """
+
+    # DB request
+    try:
+        SqliteEngine(DB_NAME).connect()
+        response = SqliteEngine(DB_NAME).select(query, (activity,))
+    finally:
+        SqliteEngine(DB_NAME).close()
+
+    # Return
+    if not response:
+        raise HTTPException(status_code=404, detail="No logs found")
+
+    return response
 
 
 
 
-
-
-# - Créer la route retournant des info sur l'activité mais avec moins d'info
 
 # - Créer la route /register broken (permet de forcer le role à admin)
 # - Créer la route /register fixed (empêche la modification de role)
