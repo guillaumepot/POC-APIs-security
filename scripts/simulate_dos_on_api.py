fb66335ba567821c82f38dd9404ae57fdbf4d292
Guillaume Pot <guillaume.pot@outlook.com>
2025-03-31 07:13:56 +0000

    Vuln4 done

---
+++ scripts/simulate_dos_on_api.py
@@ -0,0 +1,45 @@
+# scripts/simulate_dos_on_api.py
+
+
+# Lib
+import concurrent.futures
+from tqdm import tqdm
+import requests
+
+
+BASE_URL = "http://localhost:8000"
+BROKEN_ROUTE = f"{BASE_URL}/vuln4/broken/limited_route"
+FIXED_ROUTE = f"{BASE_URL}/vuln4/fixed/limited_route"
+NUM_REQUESTS = 5000
+
+def send_request(url):
+    try:
+        response = requests.get(url)
+        return response.status_code, response.json()
+    except Exception as e:
+        return "Error", str(e)
+
+
+def test_route(route, label):
+    print(f"🔄 Testing {label} ({route})...")
+    
+    success_count = 0
+    rate_limited_count = 0
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        # Utilisation de tqdm pour afficher la progression
+        results = list(tqdm(executor.map(send_request, [route] * NUM_REQUESTS), total=NUM_REQUESTS, desc=label))
+
+    for status, _ in results:
+        if status == 200:
+            success_count += 1
+        elif status == 429:
+            rate_limited_count += 1
+
+    print(f"✅ {label}: {success_count} success")
+    print(f"❌ {label}: {rate_limited_count} rate-limited (429 Too Many Requests)")
+    print("-" * 50)
+
+if __name__ == "__main__":
+    test_route(BROKEN_ROUTE, "Broken Route (No Rate Limit)")
+    test_route(FIXED_ROUTE, "Fixed Route (Rate Limited)")
--- src/app.py
+++ src/app.py
@@ -2,12 +2,17 @@
 
 # Lib
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from slowapi.errors import RateLimitExceeded
 
 from src.config.config import api_tags
 from src.routes.login import authenticator
 from src.routes.vuln1 import vulnerability1
 from src.routes.vuln2 import vulnerability2
 from src.routes.vuln3 import vulnerability3
+from src.routes.vuln4 import vulnerability4
+from src.utils.rate_limiter import rate_limiter
+
 
 """
 API Declaration
@@ -33,4 +38,16 @@ OWASP vuln routers
 app.include_router(vulnerability1, tags=["Vuln I"])
 app.include_router(vulnerability2, tags=["Vuln II"])
 app.include_router(vulnerability3, tags=["Vuln III"])
+app.include_router(vulnerability4, tags=["Vuln IV"])
 app.include_router(authenticator, tags=["Utils"])
+
+
+"""
+Utils
+"""
+# Rate limiter (used for vuln IV)
+app.state.limiter = rate_limiter
+app.add_exception_handler(RateLimitExceeded, lambda request, exc: JSONResponse(
+    status_code=429,
+    content={"detail": f"Rate limit exceeded. Rate: {exc.detail}"}
+))
